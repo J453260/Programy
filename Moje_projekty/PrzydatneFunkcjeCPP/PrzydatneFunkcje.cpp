e60e810d4a7b9cfb9aa8ea6a6fd2d25f03e6821d
Jan Bajor <jan_bajor@wp.pl>
2026-01-31 13:30:37 +0100

    Add files via upload

---
+++ Moje_projekty/PrzydatneFunkcjeCPP/PrzydatneFunkcje.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include "PrzydatneFunkcje.h"
+using namespace std;
+
+bool CzyMaPierwiastek(int a)
+{
+    int b = sqrt(a);
+    if(b*b == a)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void Trojki(int dol, int gora)
+{
+    int licznik = 0;
+    for(int i = dol; i<= gora; i++)
+    {
+        for(int j = i; j<= gora; j++)
+        {
+            int z;
+            z = i*i + j*j;
+            if(CzyMaPierwiastek(z))
+            {
+                cout<<i<<" "<<j<<" "<<sqrt(z)<<endl;
+                licznik++;
+            }
+        }
+    }
+
+    cout<<endl<<"Jest ich: "<<licznik;
+}
---
+++ Moje_projekty/PrzydatneFunkcjeCPP/PrzydatneFunkcje.h
@@ -0,0 +1,7 @@
+#ifndef PRZYDATNE_FUNKCJE
+#define PRZYDATNE_FUNKCJE
+
+bool CzyMaPierwiastek(int a);
+void Trojki(int dol, int gora);
+
+#endif // PRZYDATNE_FUNKCJE
