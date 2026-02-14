332ea4b0f121f412845a8ccb27a9d1ebc215a408
Jan Bajor <jan_bajor@wp.pl>
2026-02-14 04:17:22 +0100

    Refactor PrzydatneFunkcje.cpp for clarity

--- Moje_projekty/PrzydatneFunkcjeCPP/PrzydatneFunkcje.cpp
+++ Moje_projekty/PrzydatneFunkcjeCPP/PrzydatneFunkcje.cpp
@@ -33,19 +33,18 @@ void Trojki(int dol, int gora)
         }
     }
 
-    int NWD(int a, int b){
-        while(b != 0){
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-
-    long NWW(int a, int b){
-        return (long)a / NWD(a, b) * b;
-    }
-
     cout<<endl<<"Jest ich: "<<licznik;
 }
 
+int NWD(int a, int b){
+    while(b != 0){
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+long NWW(int a, int b){
+    return (long)a / NWD(a, b) * b;
+}
