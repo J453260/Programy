a1c45695c7dfabcdf9f690c4c5db4dcbf5fe96a8
Jan Bajor <jan_bajor@wp.pl>
2026-03-06 23:15:10 +0100

    Add files via upload

---
+++ Programy_na_uczelnie/C++/Zadanie1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T maximum(T a, T b) {
+    return (a < b) ? b : a;
+}
+
+int main(){
+    cout<<maximum(7, 3)<<endl;
+    cout<<maximum(2.71, 3.14)<<endl;
+    cout<<maximum("a", "z")<<endl;
+
+    return 0;
+}
