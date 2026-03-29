7d9f26d39212565249abd08974e9e1226eab0cc4
Jan Bajor <jan_bajor@wp.pl>
2026-03-29 09:46:42 +0200

    Refactor to find number closest to average

--- Moje_projekty/LiczbaNajbliżejŚredniej.cpp
+++ Moje_projekty/LiczbaNajbliżejŚredniej.cpp
@@ -2,33 +2,54 @@
 #include <cmath> //dla fabs
 using namespace std;
 
-//5 3 7 2 6 -> 4,6 -> 0,4 1,6 2,4 2,6 1,4
-
-float a,b,c,d,e,m,s,f,g,h,i,j,z;
-//m srednia
-//s suma
-//f najmniejsza liczba
-int main()
+float LiczbaNajblizejSredniej(float a, float b, float c, float d, float e)
 {
-	cout<<"Podaj 5 liczb oddzielonych spacja: ";
-	cin>>a>>b>>c>>d>>e;
-	s=a+b+c+d+e;
+    float m,s,f,g,h,i,j,z,liczba;
+    s=a+b+c+d+e;
 	m=s/5;
 	cout<<"Srednia podanych liczb to: "<<m<<endl;
-	
+
 	f=fabs(a-m);
 	g=fabs(b-m);
 	h=fabs(c-m);
 	i=fabs(d-m);
 	j=fabs(e-m);
-	
-	z=f;
-	if (z>g) z=g;
-	if (z>h) z=h;
-	if (z>i) z=i;
-	if (z>j) z=j;
 
-	cout<<"Liczba najblizej sredniej to: "<<m+z;
+	z=f;
+	liczba=a;
+	if (z>g)
+    {
+        z=g;
+        liczba=b;
+    }
+	if (z>h)
+    {
+        z=h;
+        liczba=c;
+    }
+	if (z>i)
+	{
+        z=g;
+        liczba=d;
+    }
+	if (z>j)
+	{
+        z=g;
+        liczba=e;
+    }
+
+	cout<<"Liczba najblizej sredniej to: "<<liczba<<endl;
+
+	return liczba;
+}
+//m srednia
+//s suma
+//z najmniejsza liczba
+int main()
+{
+	cout<<"Podaj 5 liczb oddzielonych spacja: ";
+	cin>>a>>b>>c>>d>>e;
+	cout<<"Liczba najblizej sredniej to: "<<LiczbaNajblizejSredniej(a, b, c, d, e);
 
 	return 0;
 }
