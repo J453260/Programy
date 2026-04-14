90890dab9967dcb367e9847377883528e944c1a4
Jan Bajor <jan_bajor@wp.pl>
2026-04-14 18:49:18 +0200

    Add Suma.cpp to calculate cumulative sum

---
+++ Moje_projekty/SPOJ/Łatwe/Suma.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int suma = 0;
+    int a;
+    while(cin>>a)
+    {
+        //cout<<"Podaj a: ";
+        suma+=a;
+        cout<<suma<<endl;
+    }
+
+    return 0;
+}
